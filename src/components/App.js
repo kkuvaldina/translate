eafd19dc28ed9dc4e59dc956edb4e9a73018317c
Kseniia <kuvaldina.ks@gmail.com>
2020-05-14 17:14:24 -0600

    translate is ready

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ColorContext from "../contexts/ColorContext";
+import { LanguageStore } from "../contexts/LanguageContext";
+import LanguageSelector from "./LanguageSelector";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="ui container">
+				<LanguageStore>
+					<LanguageSelector />
+					<ColorContext.Provider value="red">
+						<UserCreate />
+					</ColorContext.Provider>
+				</LanguageStore>
+			</div>
+		);
+	}
+}
+
+export default App;
